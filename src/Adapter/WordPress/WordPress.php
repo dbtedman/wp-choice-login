329c2b127e283efe686efa1d66d859df838a713f
Daniel Tedman <dbtedman@gmail.com>
2024-02-27 20:34:52 +1000

    Format fixes.

--- src/Adapter/WordPress/WordPress.php
+++ src/Adapter/WordPress/WordPress.php
@@ -6,9 +6,27 @@ namespace DBTedman\WPChoiceLogin\Adapter\WordPress;
 
 interface WordPress
 {
-    public function addAction(string $name, callable $callable, int $priority, int $argumentCount): void;
+    public function addAction(
+        string $name,
+        callable $callable,
+        int $priority,
+        int $argumentCount
+    ): void;
 
-    public function addFilter(string $name, callable $callable, int $priority, int $argumentCount): void;
+    public function addFilter(
+        string $name,
+        callable $callable,
+        int $priority,
+        int $argumentCount
+    ): void;
 
-    public function addMenuPage(string $pageTitle, string $menuTitle, string $requiredCapability, string $menuSlug, callable|null $callable, string|null $iconURL, int|null $position): void;
+    public function addMenuPage(
+        string $pageTitle,
+        string $menuTitle,
+        string $requiredCapability,
+        string $menuSlug,
+        callable|null $callable,
+        string|null $iconURL,
+        int|null $position
+    ): void;
 }
